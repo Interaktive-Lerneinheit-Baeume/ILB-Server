034dc45b0be56eb296736c823cda2b03f0dcdc7b
Alexander Bazo <alexanderbazo@googlemail.com>
2022-03-04 12:11:28 +0100

    Clean main script

--- index.js
+++ index.js
@@ -18,14 +18,12 @@ function start() {
 
 function startServer() {
   app = express();
-  // TODO:
   app.use(cors());
   app.use(express.json()); //express.json - sending data in the form of some json data object = POST or PUT
 
   app.get("/api/experiment/:id", onExperimentRequested); // Returns current state of given experiment from server
   app.get("/api/experiments/random", onRandomExperimentRequested); // Returns current state of a random pick from all available experiments
 
-  app.post("/update", onClientUpdateRequested); // Invokes client update and server restart
   app.post("/api/experiment/:id", onExperimentUpdated); // Stores current state of given experiment on server
   app.post("/api/experiment/:id/append", onExperimentDataAppended); // Stores current state of given experiment on server
   app.post("/api/experiment/:id/cancel", onExperimentCanceled); // Resets given experiment on server
@@ -37,52 +35,39 @@ function startServer() {
   );
 }
 
-function onClientUpdateRequested(request, response) {
-  //app.post("/update"
-  setImmediate(() => exec(Config.updateScript));
-  response.json({
-    msg: "Trying to update client code, server will now shutdown ...",
-  });
-}
 
 function onExperimentUpdated(request, response) {
-  //app.post("/api/experiment/:id"
+  // Called from route: app.post("/api/experiment/:id"
   let result = ExperimentManager.updateExperimentData(request.body); //updateExperimentOnDisk
   response.json(result);
 }
 
 function onExperimentDataAppended(request, response) {
-  //app.post("/api/experiment/:id/append"
+  // Called from route: app.post("/api/experiment/:id/append"
   let result = ExperimentManager.appendExperimentData(request.body); //updateClosedExperimentOnDisk
   response.json(result);
 }
 
 function onExperimentRequested(request, response) {
-  //app.get("/api/experiment/:id"
+  // Called from route: app.get("/api/experiment/:id"
   let result = ExperimentManager.getExperiment(request.params.id); //getExperiment(id)
-  console.log("id " + request.params.id);
   response.status(HTTP.OK).json(result);
 }
 
 function onRandomExperimentRequested(request, response) {
-  //app.get("/api/experiments/random"
+  // Called from route: app.get("/api/experiments/random"
   let experiment = ExperimentManager.pickRandomExperiment(); //pickRandomExperiment()
-  response.json(experiment);
+  response.status(HTTP.OK).json(experiment);
 }
 
 function onExperimentClosed(request, response) {
-  //app.post("/api/experiment/:id/close"
-
+  // Called from route: app.post("/api/experiment/:id/close"
   let result = ExperimentManager.closeExperiment(request.body); //updateExperimentOnDisk
-  if (result.msg === "404") {
-    console.log("Aber error doch!");
-  }
-
   response.json(result);
 }
 
 function onExperimentCanceled(request, response) {
-  //app.post("/api/experiment/:id/cancel"
+  // Called from route: app.post("/api/experiment/:id/cancel"
   let result = ExperimentManager.putBackExperiment(request.params.id); //resetExperimentWidthID(id)
   response.json(result);
 }
